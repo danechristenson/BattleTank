22fb0722cf5fe72fbd190ce4e2dad0eac765fa37
Dane Christenson <danechristenson@gmail.com>
2016-11-22 23:46:59 -0700

    added projectile spawn where the  barrel is pointing

--- BattleTank/Source/BattleTank/Tank.cpp
+++ BattleTank/Source/BattleTank/Tank.cpp
@@ -1,6 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "BattleTank.h"
+#include "TankBarrel.h"
+#include "Projectile.h"
 #include "TankAimingComponent.h"
 #include "Tank.h"
 
@@ -12,6 +14,7 @@ void ATank::SetTurretReference(UTankTurret * TurretToSet)
 void ATank::SetBarrelReference(UTankBarrel * BarrelToSet)
 {
 	TankAimingComponent->SetBarrelReference(BarrelToSet);
+	Barrel = BarrelToSet;
 }
 
 // Sets default values
@@ -48,4 +51,12 @@ void ATank::AimAt(FVector HitLocation)
 void ATank::Fire()
 {
 	UE_LOG(LogTemp, Warning, TEXT("Firing"));
+
+	if (!Barrel) { return; }
+
+	GetWorld()->SpawnActor<AProjectile>(
+		ProjectileBlueprint,
+		Barrel->GetSocketLocation(FName("Projectile")),
+		Barrel->GetSocketRotation(FName("Projectile"))
+		);
 }
